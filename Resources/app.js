0d5d13907d88a6458fe4fd5df4ad87c500b1e10b
Darren Cope <github@darren.cope.name>
2012-11-15 16:52:45 +0000

    intial setup

---
+++ Resources/app.js
@@ -0,0 +1,15 @@
+// All source code Copyright 2013 Cope Consultancy Services. All rights reserved
+
+
+// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+Titanium.UI.setBackgroundColor('#000');
+
+// create base root window
+//
+var win1 = Titanium.UI.createWindow({  
+    backgroundColor:'#fff'
+});
+
+/* CODE HERE */
+
+win1.open();
